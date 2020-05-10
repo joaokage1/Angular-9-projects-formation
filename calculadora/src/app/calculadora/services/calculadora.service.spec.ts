c5b6857f6516614371ad49d9ac3c7eb98bfaa8d9
joaokage1 <j_vgomes@hotmail.com>
2020-05-09 21:09:19 -0300

    create unitTests

--- calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -12,5 +12,26 @@ describe('CalculadoraService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve garantir que 1 + 5 = 6', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1,5, CalculadoraService.SOMA);
+      expect(soma).toEqual(6);
+    })
+  });
+
+  it('deve garantir que 1 - 5 = -4', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let subtracao = service.calcular(1,5, CalculadoraService.SUBTRACAO);
+      expect(subtracao).toEqual(-4);
+    })
+  });
+
+  it('deve garantir que 10 / 5 = 2', () => {
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let divisao = service.calcular(10,5, CalculadoraService.DIVISAO);
+      expect(divisao).toEqual(2);
+    })
   });
 });
--- calculadora/src/app/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,6 @@
+/**
+ * @author João Silva
+ */
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +8,39 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /**
+   * Function to calculate two numbers
+   * @param num1 
+   * @param num2 
+   * @param operacao 
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number;
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
