fd184525b3916d2600ea80fc8e12f9f68736bb2f
joaokage1 <j_vgomes@hotmail.com>
2020-05-14 12:11:01 -0300

    created model tarefa

--- gerenciador-de-tarefas/src/app/app.module.ts
+++ gerenciador-de-tarefas/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { TarefasModule } from './tarefas';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
