06f39fc706aaa0d231da6075969e9f2122a41382
joaokage1 <j_vgomes@hotmail.com>
2020-05-18 17:33:04 -0300

    created service for jogo-da-velha

--- jogo-da-velha/src/app/app.component.spec.ts
+++ jogo-da-velha/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        JogoDaVelhaModule
+      ]
     }).compileComponents();
   }));
 
